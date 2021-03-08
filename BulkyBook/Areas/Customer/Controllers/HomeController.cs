8630d18cc7a1b1f0f248902650437d4d7642c178
Rahul Chauhan <chauhan.rahul4081@gmail.com>
2021-03-09 04:05:22 +1100

    Creating and configure View_start and Imports

--- BulkyBook/Areas/Customer/Controllers/HomeController.cs
+++ BulkyBook/Areas/Customer/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using BulkyBook.Models;
+using BulkyBook.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
@@ -7,8 +8,9 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace BulkyBook.Controllers
-{
+namespace BulkyBook.Areas.Customer.Controllers
+{   
+    [Area("Customer")]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
@@ -31,7 +33,7 @@ namespace BulkyBook.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View(new Models.ViewModels.ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
