e9dfaf3384baa1e22f1fd240bb410cf0d0b0ec8b
Rahul Chauhan <chauhan.rahul4081@gmail.com>
2021-03-10 20:12:55 +1100

    repository patern

---
+++ BulkyBook.Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.Models
+{
+    public class Category
+    {   
+        [Key]
+        public int Id { get; set; }// Id if not defined explicty through data annotation as (primary key) will be by default a PK because of identity framework
+        [Display(Name="Category Name")]
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+    }
+}
